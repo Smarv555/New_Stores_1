c9b4142e97700fa7633faf6232ab53aedc37689a
Stanislav Tsanev <92050893+Smarv555@users.noreply.github.com>
2021-11-17 16:51:08 +0200

    Add files via upload

---
+++ New_Stores_1/tests.py
@@ -0,0 +1,33 @@
+import openpyxl as xl
+import os
+
+# excel_path = 'G:/My Drive/KZ_test/'
+# excel_name = 'KZ_NewStores1.xlsx'
+# excel = os.path.join(excel_path, excel_name)
+# wb = xl.load_workbook(excel)
+# print(str(wb.sheetnames[0]))
+# if wb.sheetnames[0] == 'SCANNING':
+#     print(1)
+# asd = {1: 1, 2: 2, 3: 3, 4: {1: 1}}
+#
+# x = asd.get(4)
+# y = x.get(1)
+# print(x)
+# if x:
+#     print(y)
+# else:
+#     print(0)
+#
+# x = 'asdaa'
+#
+# print(x[2:])
+
+
+string = r"VLD_BL.load_buffer ('/RMS/data/sirprdeu/UA/csrmgr/UAsirmm/','PHHTUAMMCW2455.2021-MM-011.SHOP','PHHTUAMMCW2455.2021-MM-011.DETAIL',1,1,'N','N','N',NULL,2,0);"
+
+print(string[61:])
+
+count = 1
+for i in string:
+    print(f'{count}: {i}\n')
+    count += 1
